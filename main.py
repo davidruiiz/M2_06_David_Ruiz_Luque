f8023d829db3af7f292f798b5bd2f36742ea5ce1
David Ruiz <113980722+davidruiiz@users.noreply.github.com>
2022-10-21 18:13:10 +0200

    Ejercicio 1

---
+++ main.py
@@ -0,0 +1,11 @@
+#1) Realiza una función que devuelva el área de un círculo a partir de un radio. La función se llamará area_circulo().
+#  El radio se pedirá al usuario por teclado:
+
+import math
+
+def area_circulo():
+    radio = int(input("Introduce el valor del radio:\n"))
+    area = (radio^2) * math.pi
+    return 'El área de un circulo de radio {} es de {} unidades cuadradas.'.format(radio, area)
+
+print(area_circulo())
